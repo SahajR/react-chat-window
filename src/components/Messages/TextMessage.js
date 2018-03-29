c7023cd5047babc303479711e5e29cb40067cc70
Sahaj Ramachandran <hello@sahajr.com>
2018-03-29 18:30:35 +0530

    feat(Messages): Add basic timestamp feature

--- src/components/Messages/TextMessage.js
+++ src/components/Messages/TextMessage.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import chatIconUrl from './../../assets/chat-icon.svg';
 
 const TextMessage = (props) => {
-  return <div className="sc-message--text">{props.data.text}</div>
+  return (
+    <div className="sc-message--text">{props.data.text}
+      { props.timestamp && <div className="sc-message--timestamp">{moment(props.timestamp).fromNow()}</div> }
+    </div>
+  )
 }
 
 export default TextMessage
